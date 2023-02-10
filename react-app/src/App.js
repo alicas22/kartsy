0161d2d64f2e115ec662a7b8d4e0ebb6d073a9d0
katherin <katherin.garciaVA1@gmail.com>
2023-02-10 18:03:46 -0500

    all products route and frontend store products

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -24,6 +24,9 @@ function App() {
           <Route path="/signup">
             <SignupFormPage />
           </Route>
+          <Route path='/products'>
+            
+          </Route>
         </Switch>
       )}
     </>
