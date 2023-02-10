8c2ee860fa010047e97dc7b76c34150f72062941
Ruidan-Zhang <ruidanzhang973@gmail.com>
2023-02-10 15:43:49 -0800

    reviews store completed

---
+++ react-app/src/store/products.js
@@ -0,0 +1,43 @@
+
+
+const LOAD_PRODUCTS = 'products/LOAD_PRODUCTS'
+
+
+const loadProducts = (products) => ({
+    type: LOAD_PRODUCTS,
+    products
+})
+
+export const thunkGetProducts = () => async (dispatch) => {
+    const response = await fetch('/api/products')
+
+    if(response.ok){
+        const products = await response.json()
+        dispatch(loadProducts(products))
+        return products
+    }
+}
+
+
+const normalize = (arr) => {
+    const resObj = {}
+    arr.forEach((ele) => {resObj[ele.id] = ele})
+    return resObj
+}
+
+const initialState = {}
+
+const productReducer = (state = initialState, action) => {
+    let newState
+    switch(action.type){
+        case LOAD_PRODUCTS:
+        newState = { ...state }
+        newState.allProducts = normalize(action.products)
+        console.log(newState)
+        return newState
+        default:
+            return state
+    }
+}
+
+export default productReducer
