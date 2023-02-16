9ad0a945d648eb31e84f50352cb45932a40b6b73
Anthony Licas <anthonylicas@gmail.com>
2023-02-16 13:13:41 -0800

    doubled seed data

--- app/seeds/product_images.py
+++ app/seeds/product_images.py
@@ -59,22 +59,22 @@ def seed_product_images():
 
     katherin_product2_image = ProductImage(
     product_id=12,
-    url = 'https://i.etsystatic.com/35534452/r/il/2146c2/4051469686/il_794xN.4051469686_rj95.jpg'
+    url = 'https://i.etsystatic.com/33160036/r/il/8d8a12/4248551181/il_794xN.4248551181_kws7.jpg'
     )
 
     katherin_product3_image = ProductImage(
     product_id=13,
-    url = 'https://i.etsystatic.com/26971380/r/il/702111/3483202701/il_794xN.3483202701_bj16.jpg'
+    url = 'https://i.etsystatic.com/12174691/r/il/3bd1a2/4624818231/il_794xN.4624818231_i6jl.jpg '
     )
 
     katherin_product4_image = ProductImage(
     product_id=14,
-    url = 'https://i.etsystatic.com/12174691/r/il/3bd1a2/4624818231/il_794xN.4624818231_i6jl.jpg'
+    url = 'https://i.etsystatic.com/26971380/r/il/702111/3483202701/il_794xN.3483202701_bj16.jpg'
     )
 
     katherin_product5_image = ProductImage(
     product_id=15,
-    url = 'https://i.etsystatic.com/33160036/r/il/8d8a12/4248551181/il_794xN.4248551181_kws7.jpg'
+    url = 'https://i.etsystatic.com/35534452/r/il/2146c2/4051469686/il_794xN.4051469686_rj95.jpg'
     )
 
     Sophie_product1_image = ProductImage(
@@ -101,11 +101,116 @@ def seed_product_images():
     product_id=20,
     url='https://i.etsystatic.com/7496102/r/il/df9477/3256139941/il_794xN.3256139941_124l.jpg'
     )
+##### new batch of seed data
+    anton_product21_image = ProductImage(
+    product_id=21,
+    url = 'https://www.etsy.com/img/36108299/r/il/48867d/4682129737/il_794xN.4682129737_j05j.jpg'
+    )
+
+    anton_product22_image = ProductImage(
+    product_id=22,
+    url = 'https://www.etsy.com/img/9271278/r/il/214f66/3936139899/il_794xN.3936139899_78h7.jpg'
+    )
+
+    anton_product23_image = ProductImage(
+    product_id=23,
+    url = 'https://www.etsy.com/img/28674208/r/il/80fa8f/4177697016/il_794xN.4177697016_qwr0.jpg'
+    )
+
+    anton_product24_image = ProductImage(
+    product_id=24,
+    url = 'https://www.etsy.com/img/35001350/r/il/96e0f8/4302314069/il_794xN.4302314069_qdbb.jpg'
+    )
+
+    anton_product25_image = ProductImage(
+    product_id=25,
+    url = 'https://www.etsy.com/img/5903037/r/il/570fd9/1496310818/il_794xN.1496310818_thnb.jpg'
+    )
+
+    meredith_product21_image = ProductImage(
+    product_id=26,
+    url = 'https://www.etsy.com/img/12953790/r/il/385c57/2336200012/il_794xN.2336200012_4ocm.jpg'
+    )
+
+    meredith_product22_image = ProductImage(
+    product_id=27,
+    url = 'https://www.etsy.com/img/35055979/r/il/51c589/3976585740/il_794xN.3976585740_7cu3.jpg'
+    )
+
+    meredith_product23_image = ProductImage(
+    product_id=28,
+    url = 'https://www.etsy.com/img/14217457/r/il/154077/1507211365/il_794xN.1507211365_pum8.jpg'
+    )
+
+    meredith_product24_image = ProductImage(
+    product_id=29,
+    url = 'https://www.etsy.com/img/23702777/r/il/14c918/4184624086/il_794xN.4184624086_s698.jpg'
+    )
+
+    meredith_product25_image = ProductImage(
+    product_id=30,
+    url = 'https://www.etsy.com/img/24017267/r/il/6fa8a3/2716861284/il_794xN.2716861284_q48q.jpg'
+    )
+
+    katherin_product21_image = ProductImage(
+    product_id=31,
+    url = 'https://www.etsy.com/img/11392819/r/il/751cae/1044888712/il_794xN.1044888712_mtm0.jpg'
+    )
+
+    katherin_product22_image = ProductImage(
+    product_id=32,
+    url = 'https://www.etsy.com/img/13343182/r/il/6a50b1/1598844339/il_794xN.1598844339_53j7.jpg'
+    )
+
+    katherin_product23_image = ProductImage(
+    product_id=33,
+    url = 'https://www.etsy.com/img/5485750/r/il/f9d8ee/136709953/il_794xN.136709953.jpg'
+    )
+
+    katherin_product24_image = ProductImage(
+    product_id=34,
+    url = 'https://www.etsy.com/img/13569572/r/il/9627ac/1240451515/il_794xN.1240451515_ok2h.jpg'
+    )
+
+    katherin_product25_image = ProductImage(
+    product_id=35,
+    url = 'https://www.etsy.com/img/9696294/r/il/0e5a1d/3767825094/il_794xN.3767825094_6pkx.jpg'
+    )
+
+    Sophie_product21_image = ProductImage(
+    product_id=36,
+    url= 'https://www.etsy.com/img/18224097/r/il/04097b/1818968298/il_794xN.1818968298_4cvu.jpg'
+    )
+
+    Sophie_product22_image = ProductImage(
+    product_id=37,
+    url='https://www.etsy.com/img/13920181/r/il/d9bdf4/4283905778/il_794xN.4283905778_2z5l.jpg'
+    )
+
+    Sophie_product23_image = ProductImage(
+    product_id=38,
+    url='https://www.etsy.com/img/38673016/r/il/be3cc5/4590192881/il_794xN.4590192881_64pb.jpg'
+    )
+
+    Sophie_product24_image = ProductImage(
+    product_id=39,
+    url='https://www.etsy.com/img/37883994/r/il/877d1e/4384314855/il_794xN.4384314855_2bqb.jpg'
+    )
+
+    Sophie_product25_image = ProductImage(
+    product_id=40,
+    url='https://www.etsy.com/img/23323937/r/il/565a25/2466109634/il_794xN.2466109634_5kcc.jpg'
+    )
+
 
     images = [anton_product1_image, anton_product2_image, anton_product3_image, anton_product4_image, anton_product5_image,
               meredith_product1_image, meredith_product2_image, meredith_product3_image, meredith_product4_image, meredith_product5_image,
               katherin_product1_image, katherin_product2_image, katherin_product3_image, katherin_product4_image, katherin_product5_image,
-              Sophie_product1_image, Sophie_product2_image, Sophie_product3_image, Sophie_product4_image, Sophie_product5_image]
+              Sophie_product1_image, Sophie_product2_image, Sophie_product3_image, Sophie_product4_image, Sophie_product5_image,
+              anton_product21_image, anton_product22_image, anton_product23_image, anton_product24_image, anton_product25_image,
+              meredith_product21_image, meredith_product22_image, meredith_product23_image, meredith_product24_image, meredith_product25_image,
+              katherin_product21_image, katherin_product22_image, katherin_product23_image, katherin_product24_image, katherin_product25_image,
+              Sophie_product21_image, Sophie_product22_image, Sophie_product23_image, Sophie_product24_image, Sophie_product25_image]
 
     for image in images:
         db.session.add(image)
--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -71,15 +71,101 @@ def seed_reviews():
     user4_review14 = Review(user_id=4, product_id=14, review="I recently purchased this coffee maker and I am so impressed with the quality of the coffee it produces. It's easy to use and I love the sleek design. I would definitely recommend.", star = random.randint(1,5))
     user4_review15 = Review(user_id=4, product_id=15, review="This product was a let down. It was not durable and it didn't keep my dog entertained. I wouldn't recommend.", star = random.randint(1,5))
 
-    reviews = [user1_review6,user1_review7,user1_review8,user1_review9,user1_review10,user1_review11, user1_review12, user1_review13, user1_review14, user1_review15,
-            user1_review16, user1_review17, user1_review18, user1_review19, user1_review20,user2_review1,user2_review2, user2_review3, user2_review4, user2_review5, user2_review11,
-            user2_review12, user2_review13, user2_review14, user2_review15,user2_review16, user2_review17, user2_review19,
-            user2_review18, user2_review20, user3_review1,user3_review2, user3_review3, user3_review4, user3_review5,
-            user3_review6, user3_review7, user3_review8, user3_review9, user3_review10, user3_review16,
-            user3_review17, user3_review18, user3_review19, user3_review20,
-            user4_review1, user4_review2, user4_review3, user4_review4, user4_review5, user4_review6,
-            user4_review7, user4_review8, user4_review9, user4_review10, user4_review11, user4_review12, user4_review13,
-            user4_review14, user4_review15  ]
+#### NEW SET OF SEEDER DATA
+
+    user1_review26 = Review(user_id=1, product_id=26, review="This product was a complete disappointment. It promised to deliver instant results, but it didn't do anything at all. I wouldn't recommend it to anyone.", star = random.randint(1,5))
+    user1_review27 = Review(user_id=1, product_id=27, review="I absolutely love this product! It works wonders on my skin and has helped to clear up my acne. I would definitely buy it again.", star = random.randint(1,5))
+    user1_review28 = Review(user_id=1, product_id=28, review="I was really excited to try this product, but it just didn't work for me. It left my skin feeling dry and irritated. I won't be purchasing it again.", star = random.randint(1,5))
+    user1_review29 = Review(user_id=1, product_id=29, review="This is the best product I have ever used. It's easy to apply, absorbs quickly, and leaves my skin feeling soft and moisturized.", star = random.randint(1,5))
+    user1_review30 = Review(user_id=1, product_id=30, review="I was disappointed in this product. It didn't live up to the hype and didn't do anything for my skin. I wouldn't recommend it.", star = random.randint(1,5))
+    user1_review31 = Review(user_id=1, product_id=31, review="I am absolutely in love with this product! It's made a huge difference in the texture and appearance of my skin. I would highly recommend it to anyone.", star = random.randint(1,5))
+    user1_review32 = Review(user_id=1, product_id=32, review="This product was a complete waste of money. It didn't do anything for my skin and actually made it worse. I wouldn't recommend it to anyone.", star = random.randint(1,5))
+    user1_review33 = Review(user_id=1, product_id=33, review="I was pleasantly surprised by this product. It's lightweight and easy to apply, and it made my skin look and feel great. I would definitely buy it again.", star = random.randint(1,5))
+    user1_review34 = Review(user_id=1, product_id=34, review="I was really disappointed in this product. It didn't do anything for my skin and actually made it worse. I would not recommend it.", star = random.randint(1,5))
+    user1_review35 = Review(user_id=1, product_id=35, review="This product is a game-changer! It's helped to diminish my wrinkles and fine lines, and my skin looks and feels amazing. I would recommend it to anyone.", star = random.randint(1,5))
+    user1_review36 = Review(user_id=1, product_id=36, review="I was underwhelmed by this product. It didn't do anything for my skin and left it feeling dry and irritated. I wouldn't recommend it.", star = random.randint(1,5))
+    user1_review37 = Review(user_id=1, product_id=37, review="This product is a must-have! It's made a huge difference in my skin's texture and overall appearance. I would highly recommend it to anyone.", star = random.randint(1,5))
+    user1_review38 = Review(user_id=1, product_id=38, review="I was really excited to try this product, but it just didn't work for me. It left my skin feeling oily and greasy, and I didn't see any improvements. I won't be purchasing it again.", star = random.randint(1,5))
+    user1_review39 = Review(user_id=1, product_id=39, review="This product is amazing! It's helped to brighten and even out my skin tone, and my skin looks and feels fantastic. I would definitely buy it again.", star = random.randint(1,5))
+    user1_review40 = Review(user_id=1, product_id=40, review="I was disappointed in this product. It didn't do anything for my skin and actually caused breakouts. I wouldn't recommend it.", star = random.randint(1,5))
+
+
+
+    user2_review21 = Review(user_id=2, product_id=21, review="This product was exactly what I needed. It's durable and well-made, and it has held up great over time. I would recommend it to anyone looking for a reliable option.", star = random.randint(1,5))
+    user2_review22 = Review(user_id=2, product_id=22, review="I was really disappointed with this product. It didn't work as advertised, and it was a waste of money. I wouldn't recommend it.", star = random.randint(1,5))
+    user2_review23 = Review(user_id=2, product_id=23, review="I absolutely love this product! It's so versatile and has a ton of great features. I've been using it daily and couldn't be happier with my purchase.", star = random.randint(1,5))
+    user2_review24 = Review(user_id=2, product_id=24, review="This product is a great value. It's affordable and gets the job done. I would recommend it to anyone looking for a budget-friendly option.", star = random.randint(1,5))
+    user2_review25 = Review(user_id=2, product_id=25, review="I was underwhelmed by this product. It didn't live up to the hype, and I don't think it's worth the price. I wouldn't recommend it.", star = random.randint(1,5))
+    user2_review31 = Review(user_id=2, product_id=31, review="I am so impressed with this product. It's easy to use and has exceeded my expectations. I would highly recommend it to anyone.", star = random.randint(1,5))
+    user2_review32 = Review(user_id=2, product_id=32, review="This product was a total letdown. It was difficult to use and didn't deliver the results I was hoping for. I wouldn't recommend it.", star = random.randint(1,5))
+    user2_review33 = Review(user_id=2, product_id=33, review="I can't say enough good things about this product. It's well-designed and has made my life so much easier. I would definitely buy it again.", star = random.randint(1,5))
+    user2_review34 = Review(user_id=2, product_id=34, review="I was disappointed with this product. It didn't work as advertised, and I had to return it. I wouldn't recommend it.", star = random.randint(1,5))
+    user2_review35 = Review(user_id=2, product_id=35, review="This product is a game-changer. It's made a huge difference in my daily routine and has saved me so much time. I would recommend it to anyone.", star = random.randint(1,5))
+    user2_review36 = Review(user_id=2, product_id=36, review="I was pleasantly surprised by this product. It exceeded my expectations and has become a staple in my routine. I would recommend it to anyone.", star = random.randint(1,5))
+    user2_review37 = Review(user_id=2, product_id=37, review="This product is just okay. It works, but I don't think it's anything special. I wouldn't recommend it over other similar options.", star = random.randint(1,5))
+    user2_review38 = Review(user_id=2, product_id=38, review="I am in love with this product! It's unique and has made a huge difference in my life. I would highly recommend it to anyone.", star = random.randint(1,5))
+    user2_review39 = Review(user_id=2, product_id=39, review="I was really disappointed with this product. It didn't live up to the hype, and I don't think it's worth the price. I wouldn't recommend it.", star = random.randint(1,5))
+    user2_review40 = Review(user_id=2, product_id=40, review="This product is a great investment. It's high-quality and has lasted me a long time. I would recommend it to anyone looking for a reliable option.", star = random.randint(1,5))
+
+
+    user3_review21 = Review(user_id=3, product_id=21, review="This product was a complete game-changer for me. It has all the features I need and is super easy to use. I would highly recommend it.", star = random.randint(1,5))
+    user3_review22 = Review(user_id=3, product_id=22, review="I was really disappointed with this product. It didn't work at all and was a total waste of money. I wouldn't recommend it.", star = random.randint(1,5))
+    user3_review23 = Review(user_id=3, product_id=23, review="This product is amazing. It has exceeded my expectations and has become a staple in my routine. I would recommend it to anyone.", star = random.randint(1,5))
+    user3_review24 = Review(user_id=3, product_id=24, review="I was underwhelmed by this product. It didn't live up to the hype and didn't work for me. I wouldn't recommend it.", star = random.randint(1,5))
+    user3_review25 = Review(user_id=3, product_id=25, review="This product is a great value for the price. It works well and has saved me a lot of time. I would recommend it to anyone on a budget.", star = random.randint(1,5))
+    user3_review26 = Review(user_id=3, product_id=26, review="I absolutely love this product. It has all the features I need and is super easy to use. I would recommend it to anyone.", star = random.randint(1,5))
+    user3_review27 = Review(user_id=3, product_id=27, review="I was disappointed with this product. It didn't work as advertised and was a waste of money. I wouldn't recommend it.", star = random.randint(1,5))
+    user3_review28 = Review(user_id=3, product_id=28, review="This product is a must-have. It has made a huge difference in my daily routine and has saved me so much time. I would highly recommend it.", star = random.randint(1,5))
+    user3_review29 = Review(user_id=3, product_id=29, review="I was pleasantly surprised by this product. It exceeded my expectations and has become a staple in my routine. I would recommend it to anyone.", star = random.randint(1,5))
+    user3_review30 = Review(user_id=3, product_id=30, review="This product is just okay. It works, but there are better options out there. I wouldn't recommend it over other similar products", star = random.randint(1,5))
+    user3_review36 = Review(user_id=3, product_id=36, review="I am so impressed with this product. It has all the features I need and has made my life so much easier. I would highly recommend it.", star = random.randint(1,5))
+    user3_review37 = Review(user_id=3, product_id=37, review="I was really disappointed with this product. It didn't live up to the hype and was a waste of money. I wouldn't recommend it.", star = random.randint(1,5))
+    user3_review38 = Review(user_id=3, product_id=38, review="This product is a great investment. It's high-quality and has lasted me a long time. I would recommend it to anyone looking for a reliable option.", star = random.randint(1,5))
+    user3_review39 = Review(user_id=3, product_id=39, review="I was underwhelmed by this product. It didn't work as well as I was hoping and I don't think it's worth the price. I wouldn't recommend it.", star = random.randint(1,5))
+    user3_review40 = Review(user_id=3, product_id=40, review="This product is amazing. It's well-made and has exceeded my expectations. I would recommend it to anyone looking for a high-quality option.", star = random.randint(1,5))
+
+
+    user4_review21 = Review(user_id=4, product_id=21, review="This product is a lifesaver. It has all the features I need and has made my daily routine so much easier. I would highly recommend it.", star = random.randint(1,5))
+    user4_review22 = Review(user_id=4, product_id=22, review="I was disappointed with this product. It didn't work well and was a waste of money. I wouldn't recommend it.", star = random.randint(1,5))
+    user4_review23 = Review(user_id=4, product_id=23, review="This product is amazing. It's easy to use and has exceeded my expectations. I would recommend it to anyone.", star = random.randint(1,5))
+    user4_review24 = Review(user_id=4, product_id=24, review="I was underwhelmed by this product. It didn't work as well as I was hoping and I don't think it's worth the price. I wouldn't recommend it.", star = random.randint(1,5))
+    user4_review25 = Review(user_id=4, product_id=25, review="This product is a great value for the price. It's affordable and gets the job done. I would recommend it to anyone on a budget.", star = random.randint(1,5))
+    user4_review26 = Review(user_id=4, product_id=26, review="I absolutely love this product. It's well-designed and has made a huge difference in my life. I would recommend it to anyone.", star = random.randint(1,5))
+    user4_review27 = Review(user_id=4, product_id=27, review="I was really disappointed with this product. It didn't work at all and was a total waste of money. I wouldn't recommend it.", star = random.randint(1,5))
+    user4_review28 = Review(user_id=4, product_id=28, review="This product is a must-have. It has all the features I need and has made my life so much easier. I would highly recommend it.", star = random.randint(1,5))
+    user4_review29 = Review(user_id=4, product_id=29, review="I was pleasantly surprised by this product. It exceeded my expectations and has become a staple in my routine. I would recommend it to anyone.", star = random.randint(1,5))
+    user4_review30 = Review(user_id=4, product_id=30, review="This product is just okay. It works, but there are better options out there. I wouldn't recommend it over other similar products.", star = random.randint(1,5))
+    user4_review31 = Review(user_id=4, product_id=31, review="I am so impressed with this product. It has all the features I need and has made my life so much easier. I would highly recommend it.", star = random.randint(1,5))
+    user4_review32 = Review(user_id=4, product_id=32, review="I was really disappointed with this product. It didn't live up to the hype and was a waste of money. I wouldn't recommend it.", star = random.randint(1,5))
+    user4_review33 = Review(user_id=4, product_id=33, review="This product is a great investment. It's high-quality and has lasted me a long time. I would recommend it to anyone looking for a reliable option.", star = random.randint(1,5))
+    user4_review34 = Review(user_id=4, product_id=34, review="I was underwhelmed by this product. It didn't work as well as I was hoping and I don't think it's worth the price. I wouldn't recommend it.", star = random.randint(1,5))
+    user4_review35 = Review(user_id=4, product_id=35, review="This product is amazing. It's well-designed and has exceeded my expectations. I would recommend it to anyone.", star = random.randint(1,5))
+
+    reviews = [user1_review6,user1_review7,user1_review8,user1_review9,user1_review10,
+               user1_review11, user1_review12, user1_review13, user1_review14, user1_review15,
+               user1_review16, user1_review17, user1_review18, user1_review19, user1_review20,
+               user2_review1,user2_review2, user2_review3, user2_review4, user2_review5,
+               user2_review11, user2_review12, user2_review13, user2_review14, user2_review15,
+               user2_review16, user2_review17, user2_review19,user2_review18, user2_review20,
+               user3_review1,user3_review2, user3_review3, user3_review4, user3_review5,
+               user3_review6, user3_review7, user3_review8, user3_review9, user3_review10,
+               user3_review16, user3_review17, user3_review18, user3_review19, user3_review20,
+               user4_review1, user4_review2, user4_review3, user4_review4, user4_review5,
+               user4_review6,user4_review7, user4_review8, user4_review9, user4_review10,
+               user4_review11, user4_review12, user4_review13, user4_review14, user4_review15,
+## NEW SEED DATA
+               user1_review26,user1_review27,user1_review28,user1_review29,user1_review30,
+               user1_review31, user1_review32, user1_review33, user1_review34, user1_review35,
+               user1_review36, user1_review37, user1_review38, user1_review39, user1_review40,
+               user2_review21,user2_review22, user2_review23, user2_review24, user2_review25,
+               user2_review31, user2_review32, user2_review33, user2_review34, user2_review35,
+               user2_review36, user2_review37, user2_review39,user2_review38, user2_review40,
+               user3_review21,user3_review22, user3_review23, user3_review24, user3_review25,
+               user3_review26, user3_review27, user3_review28, user3_review29, user3_review30,
+               user3_review36, user3_review37, user3_review38, user3_review39, user3_review40,
+               user4_review21, user4_review22, user4_review23, user4_review24, user4_review25,
+               user4_review26,user4_review27, user4_review28, user4_review29, user4_review30,
+               user4_review31, user4_review32, user4_review33, user4_review34, user4_review35   ]
 
     for review in reviews:
         db.session.add(review)
