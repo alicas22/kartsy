94ab451fda318b734a8a421887a4c9f567f5e050
Sophie Main <sophiekmain@gmail.com>
2023-02-16 15:13:39 -0600

    search-page-render

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -1,35 +1,51 @@
-import React, { useState, createContext } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import './Search.css'
 
 const Search = () => {
+    const history = useHistory()
 
     const searchRes = useSelector((state) => state.search)
     if (!searchRes) return null
-    const searchArr = Object.values(searchRes)
 
-    console.log('search results from component', searchArr)
-    if (!searchArr.length) return <h1>No results</h1>
+    const searchArr = Object.values(searchRes)
+    if (!searchArr.length) return <h1 className='search-title'>No results</h1>
+
+    const ProductClick = (e, id) => {
+        e.preventDefault()
+        history.push(`/products/${id}`)
+    }
 
     return searchRes && (
         <>
-            <h1> Here are your search results: </h1>
-            <ul>
-                {searchArr.map((result, index) => <li key={index}>{result.name}
-
-                </li>
-
-                 )}
-            </ul>
+            <h1 className='search-title'> Search results: </h1>
+            <div className='all-search-container'>
+                <div className='mapped-search-container'>
+                    <ul className='all-search'>
+                        {searchArr.map((result) => {
+                            return (
+                                <div className='search-card' key={result.id} onClick={(e) => ProductClick(e, result.id)}>
+                                    <div className='search-image-container'>
+                                        <img className='search-image' src={result.imagesUrl} alt='product'></img>
+                                    </div>
+                                    <div className = 'search-name-container'>
+                                        {result.name}
+                                    </div>
+                                    <div className='search-price-container'>
+                                        ${result.price.toFixed(2)}
+                                    </div>
+                                </div>
+                            )
+                        })
+                        }
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
 
-        //   <div>
-        //     {results.map((result) => (
-        //       <div key={result.id}>{result.title}</div>
-        //     ))}
-        //   </div>
-        // );
 
 
 export default Search;
